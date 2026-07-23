118c13140812660c0a19f9ce9329f19b77a3ab6a
Yeldar <edil8184@gmail.com>
2026-07-23 22:43:45 +0500

    feat: add Profile and Admin pages, fix search, set dynamic API URL

--- cv-front/src/util/api.jsx
+++ cv-front/src/util/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/v1.0";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1.0";
 
 const client = axios.create({ baseURL: BASE_URL });
 
