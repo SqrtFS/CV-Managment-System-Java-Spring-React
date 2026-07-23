1f871a44d1697d1a88de28ec0511deecbd76c54c
Yeldar <edil8184@gmail.com>
2026-07-23 23:06:40 +0500

    adjusted cors configuration

--- cv-backend/src/main/java/com/kiyulex/cv/config/SecurityConfig.java
+++ cv-backend/src/main/java/com/kiyulex/cv/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173" , "https://cvmng.kiyulex.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
